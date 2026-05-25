cc63b70f840308d03751e4fe3030cce79414f1b5
Petr Novotný <krkabol@gmail.com>
2026-05-25 07:20:58 +0200

    add datasetKey

--- htdocs/App/Console/ImportGbifRecordsCommand.php
+++ htdocs/App/Console/ImportGbifRecordsCommand.php
@@ -19,43 +19,6 @@ class ImportGbifRecordsCommand extends Command
         parent::__construct();
     }
 
-    /**
-     * ALTER TABLE IF EXISTS gbif.records DROP CONSTRAINT IF EXISTS gbif_records_taxon_fkey;
-     * DROP INDEX IF EXISTS gbif.gbif_records_coords_idx;
-     * DROP INDEX IF EXISTS gbif.gbif_records_taxon_key_idx;
-     * DROP INDEX IF EXISTS gbif.gbif_records_year_idx;
-     * DROP INDEX IF EXISTS gbif.records_institution_code_idx;
-     *
-     * -- smažeme ty jejichž taxony se nedostaly do čísleníku
-     * DELETE FROM gbif.records r WHERE NOT EXISTS (SELECT 1 FROM gbif.taxa t WHERE t.taxon_key = r.taxon_key)
-     *
-     * ALTER TABLE IF EXISTS gbif.records
-     * ADD CONSTRAINT gbif_records_taxon_fkey FOREIGN KEY (taxon_key)
-     * REFERENCES gbif.taxa (taxon_key) MATCH SIMPLE
-     * ON UPDATE NO ACTION
-     * ON DELETE CASCADE;
-     *
-     * CREATE INDEX IF NOT EXISTS gbif_records_coords_idx
-     * ON gbif.records USING gist
-     * (coords)
-     * TABLESPACE pg_default;
-     *
-     * CREATE INDEX IF NOT EXISTS gbif_records_taxon_key_idx
-     * ON gbif.records USING btree
-     * (taxon_key ASC NULLS LAST)
-     * TABLESPACE pg_default;
-     *
-     * CREATE INDEX IF NOT EXISTS gbif_records_year_idx
-     * ON gbif.records USING btree
-     * (year ASC NULLS LAST)
-     * TABLESPACE pg_default;
-     *
-     * CREATE INDEX IF NOT EXISTS records_institution_code_idx
-     * ON gbif.records USING btree
-     * (institution_code COLLATE pg_catalog."default" ASC NULLS LAST)
-     * TABLESPACE pg_default;
-     */
-
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $filePath = $this->tempDir->getPath('records.csv');
@@ -82,40 +45,67 @@ class ImportGbifRecordsCommand extends Command
         while (($row = fgetcsv($handle, null, self::SEPARATOR,chr(1), '\\')) !== false) {
 
             $data = array_combine($headers, $row);
-            foreach ($data as &$value) {
-                $value = preg_replace("/'+/", "'", $value);
-                $value = str_replace("'", "''", $value); // Escape single quotes for PostgreSQL
-            }
             $lon = (float)$data['decimalLongitude'];
             ($lon != '') ? $coords = sprintf("ST_GeomFromText('POINT(%f %f)', 4326)", (float)$data['decimalLongitude'], (float)$data['decimalLatitude']) : $coords = null;
 
-            $day = ($data['day'] != '') ? $data['day'] : null;
-            $month = ($data['month'] != '') ? $data['month'] : null;
-            $year = ($data['year'] != '') ? $data['year'] : null;
-            $precision = ($data['coordinateUncertaintyInMeters'] != '') ? $data['coordinateUncertaintyInMeters'] : null;
-            $locality = ($data['locality'] != '') ? $data['locality'] : null;
+                $sql = '
+                    INSERT INTO gbif.records (
+                        gbif_id,
+                        taxon_key,
+                        locality,
+                        recorded_by,
+                        institution_code,
+                        collection_code,
+                        coords,
+                        coords_precision,
+                        day,
+                        month,
+                        year,
+                        imported_at,
+                        taxon_rank,
+                        verbatim_scientific_name,
+                        dataset_key
+                    ) VALUES (
+                        :gbif_id,
+                        :taxon_key,
+                        :locality,
+                        :recorded_by,
+                        :institution_code,
+                        :collection_code,
+                        :coords,
+                        :coords_precision,
+                        :day,
+                        :month,
+                        :year,
+                        :imported_at,
+                        :taxon_rank,
+                        :verbatim_scientific_name,
+                        :dataset_key
+                    )
+                ';
 
-            $sql = sprintf(
-                "INSERT INTO gbif.records (gbif_id, taxon_key, locality, recorded_by,institution_code ,collection_code,coords,coords_precision, day, month, year, imported_at, taxon_rank,verbatim_scientific_name) VALUES (%d, %d, '%s','%s', '%s', '%s',%s, %d, %d,%d, %d, '%s', '%s', '%s');", //
-                $data['gbifID'],
-                $data['taxonKey'],
-                $locality,
-                $data['recordedBy'],
-                $data['institutionCode'],
-                $data['collectionCode'],
-                $coords,
-                $precision,
-                $day,
-                $month,
-                $year,
-                $importStartTime,
-                $data['taxonRank'],
-                $data['verbatimScientificName']
-            );
+                $params = [
+                    'gbif_id' => (int) $data['gbifID'],
+                    'taxon_key' => (int) $data['taxonKey'],
+                    'locality' => ($data['locality'] != '') ? $data['locality'] : null,
+                    'recorded_by' => $data['recordedBy'],
+                    'institution_code' => $data['institutionCode'],
+                    'collection_code' => $data['collectionCode'],
+                    'coords' => $coords,
+                    'coords_precision' => ($data['coordinateUncertaintyInMeters'] != '') ? $data['coordinateUncertaintyInMeters'] : null,
+                    'day' => ($data['day'] != '') ? $data['day'] : null,
+                    'month' => ($data['month'] != '') ? $data['month'] : null,
+                    'year' => ($data['year'] != '') ? $data['year'] : null,
+                    'imported_at' => $importStartTime,
+                    'taxon_rank' => $data['taxonRank'],
+                    'verbatim_scientific_name' => $data['verbatimScientificName'],
+                    'dataset_key' => $data['datasetKey'],
+                ];
 
-            try {
-
-                $this->entityManager->getConnection()->executeStatement($sql);
+                try {
+                    $this->entityManager
+                        ->getConnection()
+                        ->executeStatement($sql, $params);
             } catch (\Exception $exception) {
                 $output->writeln($sql . " \n");
                 $output->writeln($exception->getMessage() . " \n");
